d30012860e44dd0d3cc108148937287be4c44dec
Your Name <you@example.com>
2021-02-09 16:40:11 -0600

    3 - Possible Setup for Main built

--- src/aKingdomApp/Birds.java
+++ src/aKingdomApp/Birds.java
@@ -3,5 +3,39 @@ package aKingdomApp;
 public class Birds implements Animal 
 {
     //move - fly, breath - lungs, reproduce - eggs
-    
+    private String move;
+	private String breath;
+    private String reproduce;
+
+
+    public String getMove()
+    {
+    	return move;
+    }
+
+    public void setMove(String move)
+    {
+    	this.move = move;
+    }
+
+    public String getBreath()
+    {
+    	return breath;
+    }
+
+    public void setBreath(String breath)
+    {
+    	this.breath = breath;
+    }
+
+    public String getReproduce()
+    {
+    	return reproduce;
+    }
+
+    public void setReproduce(String reproduce)
+    {
+    	this.reproduce = reproduce;
+    }
+
 }
--- src/aKingdomApp/Mammals.java
+++ src/aKingdomApp/Mammals.java
@@ -3,5 +3,40 @@ package aKingdomApp;
 public class Mammals implements Animal 
 {
     //move - walk, breath - lungs, reproduce - live births
-    
+
+	private String move;
+	private String breath;
+    private String reproduce;
+
+
+    public String getMove()
+    {
+    	return move;
+    }
+
+    public void setMove(String move)
+    {
+    	this.move = move;
+    }
+
+    public String getBreath()
+    {
+    	return breath;
+    }
+
+    public void setBreath(String breath)
+    {
+    	this.breath = breath;
+    }
+
+    public String getReproduce()
+    {
+    	return reproduce;
+    }
+
+    public void setReproduce(String reproduce)
+    {
+    	this.reproduce = reproduce;
+    }
+
 }
