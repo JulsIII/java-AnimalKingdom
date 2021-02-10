05e9500a5e06be7753a61db86c54e81496d07b0b
Your Name <you@example.com>
2021-02-10 13:34:24 -0600

    8 - Birds Fixed

--- src/aKingdomApp/Birds.java
+++ src/aKingdomApp/Birds.java
@@ -3,53 +3,56 @@ package aKingdomApp;
 public class Birds extends AbstAnimals
 {
     //move - fly, breath - lungs, reproduce - eggs
-    private String move;
-	private String breath;
-    private String reproduce;
+    // private String move;
+	// private String breath;
+    // private String reproduce;
 
 
-    public Birds(int id, String name, int dyear)
+    public Birds(String name, int dyear)
     {
-        this.id = id;
-        this.dyear = dyear;
-        this.name = name;
-    }
-
-
-    public String getMove()
-    {
-    	return move;
-    }
-
-    public void setMove(String move)
-    {
-    	this.move = move;
-    }
-
-    public String getBreath()
-    {
-    	return breath;
-    }
-
-    public void setBreath(String breath)
-    {
-    	this.breath = breath;
-    }
-
-    public String getReproduce()
-    {
-    	return reproduce;
-    }
-
-    public void setReproduce(String reproduce)
-    {
-    	this.reproduce = reproduce;
+        super(name, dyear);
+        // this.id = id;
+        // this.dyear = dyear;
+        // this.name = name;
     }
 
     @Override
-    public String toString()
+    public String move()
     {
-        return "Id=" + id + ", name=" + name + " yearNamed=" + dyear;
+    	return "fly";
     }
+
+    // public void setMove(String move)
+    // {
+    // 	this.move = move;
+    // }
+
+    @Override
+    public String breath()
+    {
+    	return "lungs";
+    }
+
+    // public void setBreath(String breath)
+    // {
+    // 	this.breath = breath;
+    // }
+
+    @Override
+    public String reproduce()
+    {
+    	return "eggs";
+    }
+
+    // public void setReproduce(String reproduce)
+    // {
+    // 	this.reproduce = reproduce;
+    // }
+
+    // @Override
+    // public String toString()
+    // {
+    //     return "Id=" + id + ", name=" + name + " yearNamed=" + dyear;
+    // }
 
 }
